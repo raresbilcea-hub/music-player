dd804072525d2da9c4c2a43cc3475fa71ede70f1
Rares Bilcea <rares.bilcea@icloud.com>
2026-04-14 21:57:20 +0300

    Day 1: song library with chords and difficulty levels

---
+++ hello.js
@@ -0,0 +1,10 @@
+let song = {
+    title: "Hotel California",
+    artist: "Eagles",
+    year: 1997,
+    chords: ["Bm", "F#", "A", "E", "G", "D", "Em"],
+    difficulty: "intermediate"
+};
+console.log(song.title);
+console.log(song.chords[0]);
+console.log("Chords: " + song.chords.join(" - "));
---
+++ songs.js
@@ -0,0 +1,17 @@
+let library = [
+    { title: "My Way", artist: "Frank Sinatra", chords: ["F", "Gm", "Bb", "C", "Am", "Dm"], difficulty: "begginer" },
+    { title: "Music is the answer", artist: "Myself", chords: ["D", "A", "E"], difficulty: "begginerR" },
+    {title: "Belief", artist: "John Mayer", chords: ["Am", "Dm", "G", "Bm"], difficulty: "intermediate" },
+    {title: "Lupul", artist: "Rares Bilcea", chords: ["E", "A"], difficulty: "advanced" },
+    {title: "Freedom", artist: "Rares Bilcea", chords: ["POC", "PIC", "BAM"], difficulty: "JustBeFree" },
+
+];
+function printAll () {
+    for (let i = 0; i < library.length; i++) {
+        console.log(library[i].title + " by " + library[i].artist + " Chords: " + library[i].chords.join(" - ") +  " Level: " + library[i].difficulty )
+    };
+
+}
+printAll()
+
+
