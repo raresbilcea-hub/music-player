f25121d0f445bedeb793c6f594cb9e81f3d7949c
Rares Bilcea <rares.bilcea@icloud.com>
2026-04-18 20:44:39 +0300

    Day 3: error handling and ternary operator

--- music-search.js
+++ music-search.js
@@ -1,24 +1,39 @@
 const axios = require('axios');
 
 async function searchSong(songName) {
-  const url = "https://itunes.apple.com/search?term=" + 
-    encodeURIComponent(songName) + "&entity=song&limit=5";
-  
-  const response = await axios.get(url);
-  const data = response.data;
+  try {
+    const url = "https://itunes.apple.com/search?term=" + 
+      encodeURIComponent(songName) + "&entity=song&limit=5";
+    
+    const response = await axios.get(url);
+    const data = response.data;
 
-  console.log("=================================");
-  console.log("Results for: " + songName);
-  console.log("=================================");
-  
-  data.results.forEach(function(song, index) {
-    console.log("\n" + (index + 1) + ". " + song.trackName);
-    console.log("   Artist: " + song.artistName);
-    console.log("   Album:  " + song.collectionName);
-    console.log("   Year:   " + song.releaseDate.substring(0, 4));
-  });
+    if (data.resultCount === 0) {
+      console.log("No songs found for: " + songName);
+      return;
+    }
 
-  console.log("=================================");
+    console.log("=================================");
+    console.log("Results for: " + songName);
+    console.log("=================================");
+    
+    data.results.forEach(function(song, index) {
+      console.log("\n" + (index + 1) + ". " + song.trackName);
+      console.log("   Artist: " + song.artistName);
+      console.log("   Album:  " + song.collectionName);
+      console.log("   Year:   " + (song.releaseDate ? song.releaseDate.substring(0, 4) : "Unknown"));
+    });
+
+    console.log("=================================");
+
+  } catch (error) {
+    console.log("Something went wrong: " + error.message);
+  }
 }
 
-searchSong(process.argv[2]);
+if (!process.argv[2]) {
+  console.log("Please provide a song name!");
+  console.log("Example: node music-search.js \"Hotel California\"");
+} else {
+  searchSong(process.argv[2]);
+}
